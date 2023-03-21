773c2ce3c1fff40d62f8bd66567dd4739b510438
g33zy <stevejgaz@gmail.com>
2023-03-20 20:42:53 -0500

    final

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,16 +1,21 @@
 const isEven = (num) => {
   // write code for numbers.isEven
-
+  return num % 2 === 0 
+  
 }
 
 const sum = (arr) => {
   // write code for numbers.sum
-
+  return arr.reduce((a, c) => a + c)
 }
 
 const comboSum = (arr, sum) => {
   // write code for numbers.comboSum
-
+  for(let i = 0; i < arr.length; i++) {
+    for(let j = 0; j < arr.length; j++) {
+      return arr[i] + arr[j] === sum
+    }
+  }
 }
 
 module.exports = {
--- src/strings/index.js
+++ src/strings/index.js
@@ -1,16 +1,22 @@
 const split = (str, delim) => {
   // write code for strings.split
-
+  return str.split(delim)
 }
 
 const pairs = (str) => {
   // write code for strings.pairs
+  let results = []
+  for(let i = 0; i < str.length; i+=2) {
+    let pairs = str[i] + str[i + 1]
+   results.push(pairs)
+  }
+  return results
 
 }
 
 const reverse = (str) => {
   // write code for strings.reverse
-
+  return str.split('').reverse().join('')
 }
 
 module.exports = {
